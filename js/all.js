6fd84a07d755fa5889c9fdadf03e755b69d76b19
Ian <qq860831@gmail.com>
2020-11-09 10:33:54 +0800

    123

--- js/all.js
+++ js/all.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
   // 滑動式固定選單
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 80) {
+    if ($(this).scrollTop() > 300) {
       $(".dropdowns").addClass("navFixed");
     } else {
       $(".dropdowns").removeClass("navFixed");
     }
   });
+  //   skroller begin
+  var s = skrollr.init();
 
   // goTop
   //   $(".fa-chevron-circle-up").click(function (e) {
